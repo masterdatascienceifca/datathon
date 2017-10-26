7d0660dbc62611c80fcbe8cb689a79e3f2af3014
ferag <ferag@live.com>
2017-10-26 18:14:50 +0200

    First version for datathon

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from shapely.geometry import Point, shape
 
 from flask import Flask
 from flask import render_template
+from flask import jsonify
 import json
 
 
@@ -36,41 +37,19 @@ def get_location(longitude, latitude, provinces_json):
     return 'other'
 
 
-with open(data_path + '/geojson/china_provinces_en.json') as data_file:    
-    provinces_json = json.load(data_file)
-
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/data")
+@app.route("/data/")
+
+#This is the function that should get infor from the back-end
 def get_data():
-    gen_age_tr = pd.read_csv(data_path + 'gender_age_train.csv')
-    ev = pd.read_csv(data_path + 'events.csv')
-    ph_br_dev_model = pd.read_csv(data_path + 'phone_brand_device_model.csv')
-
-    df = gen_age_tr.merge(ev, how='left', on='device_id')
-    df = df.merge(ph_br_dev_model, how='left', on='device_id')
-    #Get n_samples records
-    df = df[df['longitude'] != 0].sample(n=n_samples)
-
-
-    top_10_brands_en = {'华为':'Huawei', '小米':'Xiaomi', '三星':'Samsung', 'vivo':'vivo', 'OPPO':'OPPO',
-                        '魅族':'Meizu', '酷派':'Coolpad', '乐视':'LeEco', '联想':'Lenovo', 'HTC':'HTC'}
-
-    df['phone_brand_en'] = df['phone_brand'].apply(lambda phone_brand: top_10_brands_en[phone_brand] 
-                                                    if (phone_brand in top_10_brands_en) else 'Other')
-
-    df['age_segment'] = df['age'].apply(lambda age: get_age_segment(age))
-
-    df['location'] = df.apply(lambda row: get_location(row['longitude'], row['latitude'], provinces_json), axis=1)
-
-    cols_to_keep = ['timestamp', 'longitude', 'latitude', 'phone_brand_en', 'gender', 'age_segment', 'location']
-    df_clean = df[cols_to_keep].dropna()
-
-    return df_clean.to_json(orient='records')
+    with open(data_path + 'boroughs.geojson') as data_file:
+        json_data = json.load(data_file)
+    return jsonify(json_data)
 
 
 if __name__ == "__main__":
---
+++ static/js/map.js
@@ -0,0 +1,19 @@
+var cupcakeTiles = L.tileLayer('http://a.tiles.mapbox.com/v3/lyzidiamond.map-ietb6srb/{z}/{x}/{y}.png', {
+    maxZoom: 18
+  });
+
+  var osmLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    maxZoom: 18
+});
+
+  $.getJSON("http://0.0.0.0:5000/data", function(data) {
+    var geojson = L.geoJson(data, {
+      onEachFeature: function (feature, layer) {
+        layer.bindPopup(feature.properties.name);
+      }
+    });
+    var map = L.map('osm-map').fitBounds(geojson.getBounds());
+    osmLayer.addTo(map);
+    geojson.addTo(map);
+  });
+
