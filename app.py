b53099ef2ac812af836d7c5d8bea05cd0048fd68
aguilarf <aguilarf@ifca.unican.es>
2017-11-10 12:39:25 +0000

    Prototype ready to release

--- app.py
+++ app.py
@@ -75,6 +75,7 @@ def get_nearest_coordinates():
     cur = conn.cursor()
     #query = """SELECT * from datathon_filtered WHERE (power((lon -""" + str(abs(longitude)) + """),2)+ power((lat - """ + str(abs(latitude)) + """),2)) <= power(""" + str(r) + """,2) AND securetype LIKE 'Opened'"""
     query = """SELECT * from datathon_filtered WHERE((ACOS(SIN(""" + str(latitude) + """ * PI() / 180) * SIN(lat * PI() / 180) + COS(""" + str(latitude) + """ * PI() / 180) * COS(lat * PI() / 180) * COS((""" + str(longitude) + """ - lon) * PI() / 180)) * 180 / PI()) * 60 * 1.1515) <""" + str(radius) +""" and securetype LIKE 'Opened'"""
+     
     print(query)
     cur.execute(query)
     rows = cur.fetchall()
@@ -90,12 +91,51 @@ def get_nearest_coordinates():
     result["features"].append(myLoc)
     for row in rows:
         result["features"].append(point_json(row[1],row[0]))    #add point to json
+    conn.close()
     return jsonify(result)
 
 def point_json(latitude,longitude):
 	result={"style": {"color": "#33cc33"}, "properties": {"marker-size": "medium", "marker-symbol": "square", "fillColor": "#33cc33", "popupContent": "Open WiFi Network"}, "geometry": {"type": "Point"}, "type": "Feature"}
 	result["geometry"]["coordinates"]=[longitude,latitude]
 	return result
+
+@app.route("/get_votes/")
+
+def get_votes_coordinates():
+    radius = 10
+    m = 0.00463 / 500                   #1m in coordinates
+    r = (radius * m) ** 2                               #radius to coordinates for the formula
+    conn = psycopg2.connect("dbname='gisdata' user='gisuser' host='193.146.75.168' password='Y8nmEHTYSZHCgc8d'")
+    cur = conn.cursor()
+    query = """SELECT * from votes"""
+    print(query)
+    cur.execute(query)
+    rows = cur.fetchall()
+    print(rows)
+
+    result = {'features': [], 'type': 'FeatureCollection'}      #dict object
+    for row in rows:
+        result["features"].append(point_json(str(row[1]),str(row[0])))    #add point to json
+    
+    conn.close()
+    return jsonify(result)
+
+@app.route("/send_vote/")
+def send_vote():
+    latitude = float(flask.request.args.get('lat'))
+    longitude = float(flask.request.args.get('long'))
+    conn = psycopg2.connect("dbname='gisdata' user='gisuser' host='193.146.75.168' password='Y8nmEHTYSZHCgc8d'")
+    cur = conn.cursor()
+    query = """ INSERT INTO votes (lon, lat) VALUES (""" + str(longitude) + """, """ + str(latitude) + """);"""
+    print(query)
+    cur.execute(query)
+    conn.commit()
+    result = {'features': [], 'type': 'FeatureCollection'}      #dict object
+    myLoc={"style": {"color": "#33cc33"}, "properties": {"marker-size": "medium", "marker-symbol": "square", "fillColor": "#33cc33", "popupContent": "Thanks for voting!"}, "geometry": {"type": "Point"}, "type": "Feature"}
+    myLoc["geometry"]["coordinates"]=[longitude,latitude]
+    result["features"].append(myLoc)
+    conn.close()
+    return jsonify(result)
 
 if __name__ == "__main__":
     context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
